41e2cbd76f02511b2c91f24236e0866a1478749b
Níkolas Escobar <40873324+NikoEscobar@users.noreply.github.com>
2019-07-02 01:06:45 -0300

    Feat Test волк

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import User from './app/models/User'; // Temporary for test[волк]
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'hello world' });
+routes.get('/', async (req, res) => {
+  // Temporary for test[волк]
+  const user = await User.create({
+    // Temporary for test[волк]
+    name: 'Wolf Test',
+    email: 'Wolf@test.com',
+    password_hash: '1234567890',
+  });
+  return res.json(user); // Temporary for test[волк]
 });
 
 export default routes;
