b6ad1713c63714c218f9294e1e8eac16fe7e94f4
Níkolas Escobar <40873324+NikoEscobar@users.noreply.github.com>
2019-07-04 20:31:04 -0300

    feat волк

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User'; // Temporary for test[волк]
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  // Temporary for test[волк]
-  const user = await User.create({
-    // Temporary for test[волк]
-    name: 'Wolf Test',
-    email: 'Wolf@test.com',
-    password_hash: '1234567890',
-  });
-  return res.json(user); // Temporary for test[волк]
-});
+routes.post('/users', UserController.store);
 
 export default routes;
