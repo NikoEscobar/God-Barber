d231c75c222c436d3adbeab2fff3d6cf8f04d203
Níkolas Escobar <40873324+NikoEscobar@users.noreply.github.com>
2020-01-25 16:41:06 -0300

    File волк

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -26,6 +26,11 @@ class User extends Model {
     return this;
   }
 
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+    //não tem tratativa de erro se a request desobedecer essa regra
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
