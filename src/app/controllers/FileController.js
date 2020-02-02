eda5438bb7b77fe2dba3080c24f28f9483d80ec4
Níkolas Escobar <40873324+NikoEscobar@users.noreply.github.com>
2020-02-02 16:19:36 -0300

    schedule волк

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -3,7 +3,9 @@ import File from '../models/File';
 class FileController {
   async store(req, res) {
     const { originalname: name, filename: path } = req.file;
-    //o upload n faz nenhuma referencia ao id de quem envia
+    /**
+     * o upload n faz nenhuma referencia ao id de quem envia
+     */
     const file = await File.create({
       name,
       path,
